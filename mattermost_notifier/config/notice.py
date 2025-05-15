ee2f6f67ba0faa9f480bf411b046978be4a13a27
Mr.Rabbit <appleseedj073@gmail.com>
2025-05-15 20:50:48 +0900

    Add files via upload

--- mattermost_notifier/config/notice.py
+++ mattermost_notifier/config/notice.py
@@ -4,7 +4,7 @@ from datetime import timezone, timedelta
 TZ = timezone(timedelta(hours=9))
 
 # Mattermost Webhook通知用
-MATTERMOST_WEBHOOK_URL = "http://172.16.0.254:8065/hooks/qbkfzrte8i8uzfofsnynm49xme"
+MATTERMOST_WEBHOOK_URL = "http://192.168.40.186:8065/hooks/4skkamks8pfx9xka7sd1st1uno"
 
 # Suricataのeve.jsonログファイルパス
 SURICATA_EVE_JSON_PATH = "/var/log/suricata/eve.json"
@@ -23,4 +23,4 @@ OPENCANARY_IP = "172.16.10.10"
 NET_INTERFACE = "wlan1"
 
 # 無活動許容時間（分）
-INACTIVITY_MINUTES = 1
+INACTIVITY_MINUTES = 2
